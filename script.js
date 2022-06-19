ca36804805b17570cbb8e001d0b4c56002d71d44
Ayo Mich <90222287+michojekunle@users.noreply.github.com>
2022-06-19 15:52:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,28 +18,3 @@ btn3.addEventListener ('click', () => {
     document.getElementById('themes').href = "themeThreeStyle.css"
 })
 
-/*------Calculations and display------*/
-
-
-//Objects holding values for all keys 
-let keyButtons = {
-    "one": '1';
-    "two": '2';
-    "three": '3';
-    "four": '4';
-    "five": '5';
-    "six": '6';
-    "seven": '7';
-    "eight": '8';
-    "nine": '9';
-    "zero":'0';
-    "decimalPoint":'.';
-    "plus":' + ';
-    "minus":' - ';
-    "times":' * ';
-    "divide":' / ';
-}
-
-let displayResult = document.getElementById('displayText').innerHTML;//screen display.
-let operation = "0";
-displayResult = operation;
