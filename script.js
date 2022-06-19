9f1e8350daef7592b7e772b580c63ee09e267bc8
Ayo Mich <90222287+michojekunle@users.noreply.github.com>
2022-06-19 14:39:32 +0100

    created object for holding the keys

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//theme switching buttons 
+
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
@@ -14,3 +16,25 @@ btn2.addEventListener ('click', () => {
 btn3.addEventListener ('click', () => {
     document.getElementById('themes').href = "themeThreeStyle.css"
 })
+
+// Calculations and display
+
+//Objects holding values for all keys 
+let keyButtons = {
+    "one": '1';
+    "two": '2';
+    "three": '3';
+    "four": '4';
+    "five": '5';
+    "six": '6';
+    "seven": '7';
+    "eight": '8';
+    "nine": '9';
+    "zero":'0';
+    "decimalPoint":'.';
+    "plus":' + ';
+    "minus":' - ';
+    "times":' * ';
+    "divide":' / ';
+}
+
