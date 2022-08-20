6d187f8f22da5fbc7665a4f3c52dd3371bdbbe6c
AMD <michojekunle1@gmail.com>
2022-08-20 17:50:29 +0100

    done with Calculator

--- script.js
+++ script.js
@@ -17,114 +17,31 @@ btn3.addEventListener ('click', () => {
     document.getElementById('themes').href = "themeThreeStyle.css"
 })
 
-/*------Calculations and display------*/
+// /*------Calculations and display------*/
+let display = document.getElementById('display');
 
+let buttons = Array.from(document.getElementsByClassName('button'));
 
-//Objects holding values for all keys 
-const keyButtons = {
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9",
-    "zero":"0",
-    "decimalPoint":".",
-    "plus":" + ",
-    "minus":" - ",
-    "times":" * ",
-    "divide":" / " ,
-    "null":" "
-}
-
-// let word = 'ei5i5 + tyhyth66';
-// console.log(word.includes(' + '));
-
-let operationArray = ['0'];
-let screenDisplay = '';
-
-displayText(operationArray)
-
-function reset() {
-    screenDisplay = "";
-    operationArray= ['0'];
-    displayText(operationArray)
-}
-
-function del() {
-    if (!(operationArray.length>1)){
-        console.log('Empty Array');
-    } else {
-        operationArray.pop();
-        screenDisplay = '';
-        displayText(operationArray);
-        console.log(operationArray);
-    }
-}
-
-function keys(key) {
-    operationArray.push(keyButtons[key]);    
-    console.log(keyButtons[key]);
-    displayText(operationArray)
-}
-
-function displayText(inputArr) {   
-    screenDisplay = '';
-    for (let i = 0; i <inputArr.length; i++) {
-        screenDisplay = screenDisplay.concat(inputArr[i]);
-    };
-    console.log(screenDisplay);
-
-    document.getElementById('displayText').innerHTML = screenDisplay;
-        // document.getElementById('displayText').innerHTML = screenDisplay.concat(inputArr[inputArr.length-1]) //screen display.                  
-}
-
-const calculateButton = document.querySelector('.equals');
-        calculateButton.addEventListener('click', () => {
-                let total;
-                //addition
-                if (screenDisplay.includes(' + ')){
-                    screenDisplay.split(' + ');
-                    console.log(screenDisplay)
-                    console.log(screenDisplay[1])
-                    total = 0;
-                    for(let i=0;i<screenDisplay.length;i+=2){
-                        total += +screenDisplay[i];
-                        console.log(total);
-                    }
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'RESET':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
                 }
-                
-                // //subtraction
-                // if (operationArray.includes(' - ')){
-                //     operationArray.split(' - ');
-                //     total;
-                //     for(let i=2;i<operationArray.length;i+=2){
-                //         parseFloat(operationArray[0]) -= parseFloat(operationArray[i]);
-                //         total = parseFloat(operationArray[0]);
-                //     }
-                //     console.log(total);
-                // }
-            
-                // //division
-                // if (operationArray.includes(' / ')){
-                //     operationArray.split(' / ');
-                //     let total;
-                //     total = parseFloat(operationArray[0]) / parseFloat(operationArray[2]);
-                // }
-            
-                // //multiplication
-                // if (screenDisplay.includes('*')){
-                //     screenDisplay.split('*');
-                //     let total = 1;
-            
-                //     for(let i=0;i<screenDisplay.length;i+=2){
-                //         total *= screenDisplay[i];
-                //     }
-                // }
-            console.log(total);
-            screenDisplay = total;
-            document.getElementById('displayText').innerHTML = screenDisplay;
-        }) 
+                break;
+            case 'DEL':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
